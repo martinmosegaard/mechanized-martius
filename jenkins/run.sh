b4594717ece0cf6e1abccf06298f202e9812d826
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-03 20:14:27 +0100

    Initial Dockerfiles

---
+++ jenkins/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker rm mechanized_martius
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock \
+              -v $(which docker):/usr/bin/docker \
+              -p 8080:8080 -p 50000:50000 \
+              --name mechanized_martius mechanized-martius/jenkins
---
+++ jenkins/terminal.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Start a shell in the docker container
+docker exec -i -t mechanized_martius bash
