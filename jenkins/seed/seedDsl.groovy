9a5fb6593c89f9732a9e0f650a5c5b9dfce30413
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-08 19:34:07 +0100

    Add test job to Job DSL

--- jenkins/seed/seedDsl.groovy
+++ jenkins/seed/seedDsl.groovy
@@ -14,8 +14,8 @@ job('commit') {
 
     cd docker
     sudo docker build -t testing-app:snapshot .
-    cid=$(sudo docker run -d --name testing-app testing-app:snapshot)
-    cip=$(sudo docker inspect --format '{{.NetworkSettings.IPAddress}}' ${cid})
+    sudo docker run -d --name testing-app testing-app:snapshot
+    cip=$(sudo docker inspect --format '{{.NetworkSettings.IPAddress}}' testing-app)
 
     # Unit test result
     sudo docker run --rm rufus/siege-engine -g http://${cip}:80/
@@ -25,6 +25,29 @@ job('commit') {
       exit 1
     else
       echo Unit test passed
+    fi
+    '''.stripIndent())
+  }
+}
+
+job('test') {
+  steps {
+    shell('''\
+    #!/bin/bash -x
+
+    # Acceptance test: Stress test for 10 seconds
+    cip=$(sudo docker inspect --format '{{.NetworkSettings.IPAddress}}' testing-app)
+    sudo docker run --rm rufus/siege-engine -t 10s http://${cip}:80/ > output 2>&1
+
+    # Acceptance test result test: Was availability 100%?
+    avail=$(cat output | grep Availability)
+    echo $avail
+    if [[ "$avail" == *"100.00"* ]]
+    then
+    	echo "Availability high enough"
+    else
+    	echo "Availability too low"
+    	exit 1
     fi
     '''.stripIndent())
   }
