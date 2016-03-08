c8f0ad6c615f7ceb4230b48c842fefb9201215b6
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-08 19:05:43 +0100

    Add commit job to Job DSL seed

--- jenkins/seed/seedDsl.groovy
+++ jenkins/seed/seedDsl.groovy
@@ -1,10 +1,26 @@
 /**
  */
 
-import javaposse.jobdsl.dsl.DslException
-
-job('foo') {
+job('commit') {
   steps {
-    shell('echo hi')
+    shell('''\
+    #!/bin/bash -x
+    sudo docker rm -f testing-app
+
+    cd docker
+    sudo docker build -t testing-app:snapshot .
+    cid=$(sudo docker run -d --name testing-app testing-app:snapshot)
+    cip=$(sudo docker inspect --format '{{.NetworkSettings.IPAddress}}' ${cid})
+
+    # Unit test result
+    sudo docker run --rm rufus/siege-engine -g http://${cip}:80/
+    if [ $? -ne 0 ]
+    then
+      echo Unit test failed
+      exit 1
+    else
+      echo Unit test passed
+    fi
+    '''.stripIndent())
   }
 }
